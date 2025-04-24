91e09f0bf5ab83ae663fa6a9914c44c98137146f
Raunak <raunak.msegs@gmail.com>
2025-04-24 20:16:31 +0530

    report frontend

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Application;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,8 +10,9 @@ class ReportController extends Controller
 {
     //
     public function index(){
-        return Inertia::render('Report/Index', [
 
+        return Inertia::render('Report/Index', [
+            'application' => Application::where('status', "asdasds")->paginate(10),
         ]);
     }
 }
