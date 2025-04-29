5f11ffad233922c70d32ce6bcc7a4d03eb0b81e7
Raunak <raunak.msegs@gmail.com>
2025-04-29 23:55:05 +0530

    first build

---
+++ public/build/assets/useStudyTourApplicationStore-C6K5c3Vw.js
@@ -0,0 +1,1 @@
+import{C as t}from"./app-DwjiYckU.js";const a=t("application",{state:()=>({type:"STUDY TOUR",status:"Pending",applicant_name:"",gender:"",designation:"",contact:"",study_tour_details:{institute:"",institute_approval:null,male:"",female:""},location:"",start_date:"",end_date:""}),actions:{reset(){this.$reset()}},persist:!0});export{a as u};
