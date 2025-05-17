73f35a6bcb35af46a827e666f09a63a386e1abd4
Raunak <raunak.msegs@gmail.com>
2025-05-17 21:41:31 +0530

    login without digilocker

--- resources/js/Pages/Frontend/Auth/Login.vue
+++ resources/js/Pages/Frontend/Auth/Login.vue
@@ -41,38 +41,12 @@
             <button  type="submit" class="mt-6 w-60 bg-black text-white py-2 rounded-md hover:bg-black mx-auto block">Login</button>
         </form>
 
-        <form
-            ref="digilockerRef"
-            class="hidden"
-            method="get"
-            action="https://digilocker.meripehchaan.gov.in/public/oauth2/1/authorize"
-            id="digilockerForm"
-        >
-            <input v-model="signerForm.response_type" type="hidden" name="response_type" />
-            <input v-model="signerForm.client_id" type="hidden" name="client_id" />
-            <input v-model="signerForm.redirect_uri" type="hidden" name="redirect_uri" />
-            <input v-model="signerForm.state" type="hidden" name="state" />
-            <input v-model="signerForm.scope" type="hidden" name="scope" />
-            <input v-model="signerForm.code_challenge" type="hidden" name="code_challenge" />
-            <input v-model="signerForm.code_challenge_method" type="hidden" name="code_challenge_method" />
-        </form>
-
-
-
-
-
-<!--        <button @click="handleDigilocker" class=" mt-5 bg-black text-white flex ml-10 items-center p-3 rounded-lg shadow-md hover:bg-primary/80 whitespace-nowrap">-->
-<!--            <img aria-hidden="true" alt="DigiLocker icon" src="/images/icons/Digilocker_logo.png" class="mr-3 w-[120px]" />-->
-<!--            Continue with DigiLocker-->
-<!--        </button>-->
-
 
         <div class="mt-4 flex items-center justify-between">
             <a href="#" class="text-muted-foreground text-start">Register New Account!</a>
 
             <a href="#" class="text-muted-foreground text-end">Forgot Password?</a>
         </div>
-
 
 
     </div>
@@ -102,9 +76,6 @@ defineProps({
     error: String
 });
 
-
-const digilockerRef = ref(null);
-
 const showError = ref(false)
 const handleSubmit=e=>{
     form.post(route('login.store'),{
@@ -118,46 +89,8 @@ watch(
     },
     { immediate: true }
 )
-const signerForm=reactive({
-    response_type: null,
-    client_id: null,
-    redirect_uri: null,
-    state: null,
-    scope: null,
-    code_challenge: null,
-    code_challenge_method: null,
-})
 
-const handleDigilocker = async () => {
-    try {
-        const res = await axios.get(route('digilocker.params'))
-        const {
-            response_type,
-            client_id,
-            redirect_uri,
-            state,
-            scope,
-            code_challenge,
-            code_challenge_method,
-        } = res.data
 
-        signerForm.response_type=  (response_type);
-        signerForm.client_id=(client_id);
-        signerForm.redirect_uri=(redirect_uri);
-        signerForm.state=(state);
-        signerForm.scope=(scope);
-        signerForm.code_challenge=(code_challenge);
-        signerForm.code_challenge_method=(code_challenge_method);
-
-        // Wait for DOM updates to apply form data
-        await nextTick()
-
-        digilockerRef.value.submit();
-
-    } catch (error) {
-        console.error('Error getting Digilocker params:', error)
-    }
-}
 
 </script>
 
