4fe184bcca1be0c7a0eee426ecc8854734e61454
Raunak <raunak.msegs@gmail.com>
2025-05-18 15:37:27 +0530

    new build

--- public/build/assets/useStudyTourApplicationStore-BjiJ_oeQ.js
+++
@@ -1,1 +0,0 @@
-import{J as t}from"./app-BUTAtcIn.js";const a=t("study_tour_application",{state:()=>({type:"STUDY TOUR",status:"Pending",applicant_name:"",gender:"",designation:"",contact:"",study_tour_details:{institute:"",institute_approval:null,male:"",female:""},location:"",start_date:"",end_date:"",state_id:""}),actions:{reset(){this.$reset()}},persist:!0});export{a as u};
