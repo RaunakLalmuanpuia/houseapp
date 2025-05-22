8410a9fc12d104793ddca46bb0cf3046c8d7129b
Raunak <raunak.msegs@gmail.com>
2025-05-23 02:41:35 +0530

    create user seeder for all houses

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,37 +2,63 @@
 
 namespace Database\Seeders;
 
+use App\Models\House;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
-//        User::create([
-//            'name' => 'Admin User',
-//            'email' => 'admin@mail.com',
-//            'password' => Hash::make('password'), // use bcrypt or Hash::make
-//        ]);
+        // Admin user
         User::query()->upsert([
-            ['id'=>1,'name'=>'admin','email'=>'admin@mail.com','password'=>Hash::make('password')],
-            ['id'=>2,'name'=>'salt lake','email'=>'house1@mail.com','password'=>Hash::make('password')],
+            [
+                'id' => 1,
+                'name' => 'GAD admin',
+                'email' => 'admin@mail.com',
+                'password' => Hash::make('password'),
+                'mobile' => $this->generateMobile(),
+            ]
         ], ['id']);
 
-        $admin=User::query()->find(1);
+        User::query()->upsert([
+            [
+                'id' => 2,
+                'name' => 'Msegs admin',
+                'email' => 'msegs@mail.com',
+                'password' => Hash::make('password'),
+                'mobile' => $this->generateMobile(),
+            ]
+        ], ['id']);
+
+        $admin = User::query()->find(1);
         $admin->assignRole('Admin');
 
-        $house=User::query()->find(2);
-        $house->assignRole('House');
-        $house->house_id = 1;
-        $house->save();
+        // Users for each house
+        $houses = House::all();
 
+        foreach ($houses as $house) {
+            $username = strtolower(str_replace(' ', '_', $house->name));
+            $email = $username . '@mail.com';
+
+            $user = User::updateOrCreate(
+                ['email' => $email],
+                [
+                    'name' => $house->name,
+                    'password' => Hash::make('password'),
+                    'house_id' => $house->id,
+                    'mobile' => $this->generateMobile(),
+                ]
+            );
+
+            $user->assignRole('House');
+        }
+    }
+
+    private function generateMobile(): string
+    {
+        return '9' . mt_rand(100000000, 999999999); // ensures a 10-digit mobile starting with 9
     }
 }
